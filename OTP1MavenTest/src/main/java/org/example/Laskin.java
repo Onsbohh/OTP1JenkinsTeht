d51945f00a1935c79b4422726aa7c2fc446716b6
Onsbohh <104371139+Onsbohh@users.noreply.github.com>
2023-09-18 11:51:38 +0300

    Add files via upload

---
+++ OTP1MavenTest/src/main/java/org/example/Laskin.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Laskin {
+    double tulos;
+    public double summa(double n, double i) {
+        return n + i;
+    }
+
+    public double kerro(double n, double i) {
+        return n * i;
+    }
+
+    public double miinus(double n, double i) {
+        return n - i;
+    }
+}
